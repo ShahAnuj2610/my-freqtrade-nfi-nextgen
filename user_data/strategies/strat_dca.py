705af60aac91409819c8fbb5b8783f1efea8a8ae
anujshah <anujshah584@gmail.com>
2021-12-24 10:32:29 +0530

    use strat_dca.py

---
+++ user_data/strategies/strat_dca.py
@@ -0,0 +1,66 @@
+import math
+import logging
+from datetime import datetime
+
+from freqtrade.persistence import Trade
+from freqtrade.strategy import IntParameter
+
+from user_data.strategies.tbedit import tbedit
+
+logger = logging.getLogger(__name__)
+
+
+class strat_dca(tbedit):
+    initial_safety_order_trigger = -0.057
+    max_safety_orders = 2
+    safety_order_step_scale = 2
+    safety_order_volume_scale = 2
+
+    buy_params = {
+        "dca_min_rsi": 36,
+    }
+
+    dca_min_rsi = IntParameter(35, 75, default=buy_params['dca_min_rsi'], space='buy', optimize=True)
+
+    def adjust_trade_position(self, pair: str, trade: Trade,
+                              current_time: datetime, current_rate: float, current_profit: float,
+                              **kwargs):
+        if current_profit > self.initial_safety_order_trigger:
+            return None
+
+        # credits to reinuvader for not blindly executing safety orders
+        # Obtain pair dataframe.
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+        # Only buy when it seems it's climbing back up
+        last_candle = dataframe.iloc[-1].squeeze()
+        if last_candle['rsi'] < self.dca_min_rsi.value:
+            logger.info(f"DCA for {pair} waiting for RSI({last_candle['rsi']}) to rise above {self.dca_min_rsi.value}")
+            return None
+
+        count_of_buys = 0
+        for order in trade.orders:
+            if order.ft_is_open or order.ft_order_side != 'buy':
+                continue
+            if order.status == "closed":
+                count_of_buys += 1
+
+        if 1 <= count_of_buys <= self.max_safety_orders:
+
+            safety_order_trigger = abs(self.initial_safety_order_trigger) + (
+                    abs(self.initial_safety_order_trigger) * self.safety_order_step_scale * (
+                    math.pow(self.safety_order_step_scale, (count_of_buys - 1)) - 1) / (
+                            self.safety_order_step_scale - 1))
+
+            if current_profit <= (-1 * abs(safety_order_trigger)):
+                try:
+                    stake_amount = self.wallets.get_trade_stake_amount(pair, None)
+                    stake_amount = stake_amount * math.pow(self.safety_order_volume_scale, (count_of_buys - 1))
+                    amount = stake_amount / current_rate
+                    logger.info(
+                        f"Initiating safety order buy #{count_of_buys} for {pair} with stake amount of {stake_amount} which equals {amount}")
+                    return stake_amount
+                except Exception as exception:
+                    logger.info(f'Error occured while trying to get stake amount for {pair}: {str(exception)}')
+                    return None
+
+        return None
