8088124b32c6541a3c4c2c928dfac91bbc5170f9
anujshah <anujshah584@gmail.com>
2023-09-05 09:29:57 +0000

    New release version

--- user_data/strategies/NostalgiaForInfinityX.py
+++ user_data/strategies/NostalgiaForInfinityX.py
@@ -10,7 +10,7 @@
     <h1>Error 503 first byte timeout</h1>
     <p>first byte timeout</p>
     <h3>Error 54113</h3>
-    <p>Details: cache-iad-kcgs7200073-IAD 1693905696 2936771894</p>
+    <p>Details: cache-dfw-kdal2120081-DFW 1693906150 1615937700</p>
     <hr>
     <p>Varnish cache server</p>
   </body>
